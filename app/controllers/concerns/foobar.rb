3a091e1e4547f360b27ff2dba8a42e448f69da93
Linda He <he.linda.j@gmail.com>
2015-09-26 01:29:50 +0000

    hw2 finished

--- app/controllers/concerns/foobar.rb
+++ app/controllers/concerns/foobar.rb
@@ -1,3 +1,9 @@
 class Foobar
   # ENTER CODE FOR Q2 HERE
+  def initialize(text)
+     @text = text
+  end
+  def bar(a, optional={})
+     "#{a}#{@text}#{optional[:sat]}"
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,16 +1,24 @@
 class PagesController < ApplicationController
   def home
-    # foo = Foobar.new "baz"
-    # @baz = foo.bar :cat, sat: :dat, dat: :sat
+    foo = Foobar.new "baz"
+    @baz = foo.bar :cat, sat: :dat, dat: :sat
   end
 
   def stringify
-    @text = "You are nothing!"
+    if params[:name].length > 0 and params[:adjective].length > 0 
+    	@text = params[:name] + " is so " + params[:adjective]
+    else
+	@text = "You are nothing!"
+    end
   end
 
   def age
   end
 
   def person
+    linda = Person.new params[:name], params[:age]
+    @introduction = linda.introduce()
+    @birth_year = linda.birth_year()
+    @nickname = linda.nickname()
   end
 end
